ba8eee87bc3945667bf846758cef7100c3b2971c
Iván Navarro <36862204+Ivannavarrogar@users.noreply.github.com>
2023-10-14 21:24:57 +0200

    Cajal
    Movimiento de cajal terminado

--- RamonYCajal/Assets/Scripts/CajalController.cs
+++ RamonYCajal/Assets/Scripts/CajalController.cs
@@ -8,30 +8,61 @@ public class CajalController : MonoBehaviour
 
     public float JumpForce = 1;
 
+    private bool _onFloor;
+
     private Rigidbody2D _rigidbody;
+    private Animator _animator;
 
     void Start()
-
     {
-
         _rigidbody = GetComponent<Rigidbody2D>();
-
+        _animator = GetComponent<Animator>();
     }
 
     void Update()
-
     {
+        //Horizontal movement
+         if (Input.GetKey(KeyCode.A))
 
-        var movement = Input.GetAxis("Horizontal");
+         {
+             _rigidbody.velocity = new Vector2(-MovementSpeed, _rigidbody.velocity.y);
+         }
+         if (Input.GetKey(KeyCode.D))
 
-        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
-
-        if (Input.GetButtonDown("Jump") )
+         {
+             _rigidbody.velocity = new Vector2(MovementSpeed, _rigidbody.velocity.y);
+         }
+        _animator.SetFloat("Horizontal", Mathf.Abs(_rigidbody.velocity.x)); //Sets the state of run in the animator
+     
+        //Vertical movement
+        _onFloor = _rigidbody.velocity.y == 0;//Checks if the player is on the floor
+        if (Input.GetKeyDown(KeyCode.W) && _onFloor)
 
         {
 
             _rigidbody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
 
         }
+        _animator.SetBool("OnFloor", _onFloor);//sets the jump state in the animator
+
+        //Flips the character to face the movement direction
+        if (_rigidbody.velocity.x >0)
+        {
+            transform.eulerAngles = new Vector3(0,0,0);
+        }
+        if (_rigidbody.velocity.x < 0)
+        {
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        }
+
+        /* if (Input.GetKey(KeyCode.A))
+         {
+             _rigidbody.AddForce(new Vector2(-MovementSpeed,0)*Time.deltaTime);
+         }
+         if (Input.GetKey(KeyCode.D))
+
+         {
+             _rigidbody.AddForce(new Vector2(MovementSpeed, 0) * Time.deltaTime);
+         }*/
     }
 }
