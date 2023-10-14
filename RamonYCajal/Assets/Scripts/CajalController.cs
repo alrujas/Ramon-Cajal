36813f5c07b2ea782df736ab9cb763b3bf3f6568
Iván Navarro <36862204+Ivannavarrogar@users.noreply.github.com>
2023-10-14 16:55:44 +0200

    Proyecto unity dentro

---
+++ RamonYCajal/Assets/Scripts/CajalController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CajalController : MonoBehaviour
+{
+    public float MovementSpeed = 1;
+
+    public float JumpForce = 1;
+
+    private Rigidbody2D _rigidbody;
+
+    void Start()
+
+    {
+
+        _rigidbody = GetComponent<Rigidbody2D>();
+
+    }
+
+    void Update()
+
+    {
+
+        var movement = Input.GetAxis("Horizontal");
+
+        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;
+
+        if (Input.GetButtonDown("Jump") )
+
+        {
+
+            _rigidbody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+
+        }
+    }
+}
