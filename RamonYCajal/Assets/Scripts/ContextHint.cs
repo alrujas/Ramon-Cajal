5f76b2614ee3700bae963a9be45a626c8f7321c7
Iván Navarro <36862204+Ivannavarrogar@users.noreply.github.com>
2023-12-23 14:50:36 +0100

    Pistas contexto accesibilidad y problemas del nivel

---
+++ RamonYCajal/Assets/Scripts/ContextHint.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContextHint : MonoBehaviour
+{ 
+     /// <summary>
+    /// Gamemanager Object
+    /// </summary>
+    private GameManager _gm;
+
+    [SerializeField] private GameObject _hint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _gm = GameManager.Instance;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Cajal") /*&& _gm. accesibilidad opcion true*/)
+        {
+            _hint.SetActive(true);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        _hint.SetActive(false);
+    }
+
+}
